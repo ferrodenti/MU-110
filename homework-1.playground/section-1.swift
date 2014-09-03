ee7c129056a35cbf76746e18ecaeb1fb335f1149
ferrodenti <radgorov@gmail.com>
2014-09-04 03:17:27 +0400

    ident fix

--- homework-1.playground/section-1.swift
+++ homework-1.playground/section-1.swift
@@ -6,27 +6,27 @@ Homework 1 - fibonacci sequence
 */
 
 func fibonacci(n:Int) -> (Int,Int) {
-	
-	switch n {
-		
-	case let x where x < 0: // For negative N: F(-n) = (-1)pow(n+1) * (Fn)
-		let fn = fibonacci(-n).1
-		return (n, n%2==0 ? -fn : fn)
-		
-	case 0,1:
-		return (n,n)
-	
-	default:
-		var prev = 0
-		var val = 1
-	
-		for i in 2...n {
-			let tmp = val
-			val += prev
-			prev = tmp
-		}
-		return (n, val)
-	}
+  
+  switch n {
+    
+  case let x where x < 0: // For negative N: F(-n) = (-1)pow(n+1) * (Fn)
+    let fn = fibonacci(-n).1
+    return (n, n%2==0 ? -fn : fn)
+    
+  case 0,1:
+    return (n,n)
+    
+  default:
+    var prev = 0
+    var val = 1
+    
+    for i in 2...n {
+      let tmp = val
+      val += prev
+      prev = tmp
+    }
+    return (n, val)
+  }
 }
 
 let testValue = 10
